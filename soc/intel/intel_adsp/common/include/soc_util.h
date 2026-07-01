11a11a89ed5180e6eb1a8c2ff69b39e0b91b5a37
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2026-07-01 11:44:15 +0100

    [xtensa] Enable early boot CPENABLE initialization and LLVM compatibility fixes

--- soc/intel/intel_adsp/common/include/soc_util.h
+++ soc/intel/intel_adsp/common/include/soc_util.h
@@ -11,12 +11,29 @@
 /* memcopy used by boot loader */
 static ALWAYS_INLINE void bmemcpy(void *dest, void *src, size_t bytes)
 {
-	uint8_t *d = (uint8_t *)dest;
-	uint8_t *s = (uint8_t *)src;
-
 	sys_cache_data_invd_range(src, bytes);
-	for (size_t i = 0; i < bytes; i++) {
-		d[i] = s[i];
+
+	if ((((uintptr_t)dest | (uintptr_t)src) & 3) == 0) {
+		uint32_t *d32 = (uint32_t *)dest;
+		uint32_t *s32 = (uint32_t *)src;
+		size_t words = bytes / 4;
+		size_t remainder = bytes % 4;
+
+		for (size_t i = 0; i < words; i++) {
+			d32[i] = s32[i];
+		}
+
+		uint8_t *d8 = (uint8_t *)(d32 + words);
+		uint8_t *s8 = (uint8_t *)(s32 + words);
+		for (size_t i = 0; i < remainder; i++) {
+			d8[i] = s8[i];
+		}
+	} else {
+		uint8_t *d8 = (uint8_t *)dest;
+		uint8_t *s8 = (uint8_t *)src;
+		for (size_t i = 0; i < bytes; i++) {
+			d8[i] = s8[i];
+		}
 	}
 
 	sys_cache_data_flush_range(dest, bytes);
@@ -24,10 +41,17 @@ static ALWAYS_INLINE void bmemcpy(void *dest, void *src, size_t bytes)
 
 static ALWAYS_INLINE void bbzero(void *dest, size_t bytes)
 {
-	uint8_t *d = (uint8_t *)dest;
+	uint32_t *d32 = (uint32_t *)dest;
+	size_t words = bytes / 4;
+	size_t remainder = bytes % 4;
 
-	for (size_t i = 0; i < bytes; i++) {
-		d[i] = 0;
+	for (size_t i = 0; i < words; i++) {
+		d32[i] = 0;
+	}
+
+	uint8_t *d8 = (uint8_t *)(d32 + words);
+	for (size_t i = 0; i < remainder; i++) {
+		d8[i] = 0;
 	}
 
 	sys_cache_data_flush_range(dest, bytes);
